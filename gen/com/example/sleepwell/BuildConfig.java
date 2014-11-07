d805d86f56a8b95b22219ea61a7cc4bac8c4a662
hoavt-54 <myfullhouse.6789@gmail.com>
2014-11-07 22:43:48 +0700

    very first commit

---
+++ gen/com/example/sleepwell/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.sleepwell;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/org/tomato/sleepwell/SleepWellService.java
@@ -0,0 +1,17 @@
+package org.tomato.sleepwell;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class SleepWellService extends Service{
+
+	
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
